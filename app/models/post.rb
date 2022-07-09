31f53dc7342fdbe980344285488def3b7771db1d
Aaron Noe <aarminoe@gmail.com>
2022-07-09 12:59:06 -0500

    commit tree a78610614e21851488d593e9da97ee16245ad386

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,28 @@
 class Post < ApplicationRecord
+   validates :title, presence: true
+   validates :content, length: {minimum: 250}
+   validates :summary, length: {maximum: 250}
+   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+   validate :clickbait
+
+   def clickbait
+        good_title = [/Won't Believe/, /Top [number]/, /Guess/, /Secret/]   
+        if good_title.none? {|t| t.match (title)} 
+            errors.add(:title, "nope!")
+        end
+    end
+
+
+    # def clickbait
+    #     good_title = ["Won't Believe", "Top [number]", "Guess", "Secret"]  
+    #     good_title.each do |t|
+    #         if !title.include? t
+    #             errors.add(:title, "nope!")
+    #         end
+    #     end
+    #     # unless title.include? "Won't Believe", "Top [number]", "Guess", "Secret"
+    #     #     errors.add(:title, "nope!")
+    #     # end
+    # end
+
 end
